66e3a6beabf8e533a4cfe9716ad5961cdee617a5
devriansyah <dvr.syh@gmail.com>
2021-01-16 15:33:28 +0700

    Init be-crowfunding, up enpoint register-user

---
+++ user/formatter.go
@@ -0,0 +1,23 @@
+package user
+
+// UserFormatter is ...
+type UserFormatter struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Occupation string `json:"occupation"`
+	Email      string `json:"email"`
+	Token      string `json:"token"`
+}
+
+// FormatUser is ...
+func FormatUser(user User, token string) UserFormatter {
+	formatter := UserFormatter{
+		ID:         user.ID,
+		Name:       user.Name,
+		Occupation: user.Occupation,
+		Email:      user.Email,
+		Token:      token,
+	}
+
+	return formatter
+}
