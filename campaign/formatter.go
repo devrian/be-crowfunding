98077981d9f553bc390a065c14bea6230511df35
devriansyah <dvr.syh@gmail.com>
2021-02-13 12:54:10 +0700

    up endpoint campaign detail

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,5 +1,7 @@
 package campaign
 
+import "strings"
+
 // CampaignFormatter is ...
 type CampaignFormatter struct {
 	ID               int    `json:"id"`
@@ -12,24 +14,52 @@ type CampaignFormatter struct {
 	Slug             string `json:"slug"`
 }
 
+// CampaignDetailFormatter is ...
+type CampaignDetailFormatter struct {
+	ID               int                      `json:"id"`
+	UserID           int                      `json:"user_id"`
+	Name             string                   `json:"name"`
+	ShortDescription string                   `json:"short_description"`
+	ImageURL         string                   `json:"image_url"`
+	GoalAmount       int                      `json:"goal_amount"`
+	CurrentAmount    int                      `json:"current_amount"`
+	Slug             string                   `json:"slug"`
+	Description      string                   `json:"description"`
+	Perks            []string                 `json:"perks"`
+	User             CampaignUserFormatter    `json:"user"`
+	Images           []CampaignImageFormatter `json:"images"`
+}
+
+// CampaignUserFormatter is ...
+type CampaignUserFormatter struct {
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
+}
+
+// CampaignImageFormatter is ...
+type CampaignImageFormatter struct {
+	ImageURL  string `json:"image_url"`
+	IsPrimary bool   `json:"is_primary"`
+}
+
 // FormatCampaign is ...
 func FormatCampaign(campaign Campaign) CampaignFormatter {
-	formatter := CampaignFormatter{
+	campaigFormatter := CampaignFormatter{
 		ID:               campaign.ID,
 		UserID:           campaign.UserID,
 		Name:             campaign.Name,
 		ShortDescription: campaign.ShortDescription,
+		ImageURL:         "",
 		GoalAmount:       campaign.GoalAmount,
 		CurrentAmount:    campaign.CurrentAmount,
-		ImageURL:         "",
 		Slug:             campaign.Slug,
 	}
 
 	if len(campaign.CampaignImages) > 0 {
-		formatter.ImageURL = campaign.CampaignImages[0].FileName
+		campaigFormatter.ImageURL = campaign.CampaignImages[0].FileName
 	}
 
-	return formatter
+	return campaigFormatter
 }
 
 // FormatCampaigns is ...
@@ -42,4 +72,51 @@ func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
 	}
 
 	return campaignsFormatter
+}
+
+// FormatCampaignDetail is ...
+func FormatCampaignDetail(campaign Campaign) CampaignDetailFormatter {
+	campaignDetailFormatter := CampaignDetailFormatter{
+		ID:               campaign.ID,
+		UserID:           campaign.UserID,
+		Name:             campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		ImageURL:         "",
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+		Slug:             campaign.Slug,
+		Description:      campaign.Description,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignDetailFormatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	// mapping perks
+	var perks []string
+	for _, perk := range strings.Split(campaign.Perks, ", ") {
+		perks = append(perks, perk)
+	}
+	campaignDetailFormatter.Perks = perks
+
+	// mapping user
+	user := campaign.User
+	campaignUserFormatter := CampaignUserFormatter{
+		Name:     user.Name,
+		ImageURL: user.AvatarFileName,
+	}
+	campaignDetailFormatter.User = campaignUserFormatter
+
+	// mapping images
+	images := []CampaignImageFormatter{}
+	for _, image := range campaign.CampaignImages {
+		campaignImageFormatter := CampaignImageFormatter{
+			ImageURL:  image.FileName,
+			IsPrimary: image.IsPrimary == 1,
+		}
+		images = append(images, campaignImageFormatter)
+	}
+	campaignDetailFormatter.Images = images
+
+	return campaignDetailFormatter
 }
